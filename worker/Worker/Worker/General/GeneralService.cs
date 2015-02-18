1c4f908e7f32e5c8f13e4bf8681c71c37680b169
xnivel <xnivel@gmail.com>
2015-02-18 22:55:59 +0100

    status added1

--- worker/Worker/Worker/General/GeneralService.cs
+++ worker/Worker/Worker/General/GeneralService.cs
@@ -6,7 +6,7 @@ namespace Worker
 	public class GeneralService: Service
 	{
 		public object Any(GetStatus request){
-			SystemLoadChecker systemChecker = new SystemLoadChecker ();
+			//SystemLoadChecker systemChecker = new SystemLoadChecker ();
 			//return new GetStatusResponse { Result = systemChecker.getAvailableRAM()+systemChecker.getCurrentCpuUsage()};
 			return new GetStatusResponse { CPUproc=GeneralServiceUtils.getCPUproc(),MEMproc=GeneralServiceUtils.getMemproc()};
 		}
